c3955395c646bfa3e900483d568c3b4e7a26d077
Sergio García <garciparedes@gmail.com>
2015-10-20 15:46:10 +0200

    Presentation

--- src/Main.py
+++ src/Main.py
@@ -18,17 +18,11 @@ from mpl_toolkits.mplot3d import Axes3D
 
 
 def main():
+    multidim()
 
 
     space = Space.generateSpace(10000,3,10**(-10))
     print space
-
-    closestPairDivide(space)
-    closestPairBrutePlus(space)
-    print "\n\n\n"
-
-
-
 
     fig = pylab.figure()
     ax = Axes3D(fig)
@@ -37,17 +31,27 @@ def main():
     for i in space.pointList:
         ax.scatter(i.vector[0], i.vector[1], i.vector[2], s= 30)
 
+    closestPair = closestPairBrutePlus(space)
+
     print "\n\n\n"
 
+    closestPair = closestPairDivide(space)
 
-    closestPair = space.getClosestDivideConquer()
-    closestPairBrutePlus(space)
-
-    print closestPair
     ax.scatter(closestPair.pointA.vector[0], closestPair.pointA.vector[1], closestPair.pointA.vector[2], c= 'r', s= 30)
     ax.scatter(closestPair.pointB.vector[0], closestPair.pointB.vector[1], closestPair.pointB.vector[2], c= 'r', s= 30)
     pyplot.show()
 
+
+
+
+
+def multidim():
+    space2 = Space.generateSpace(10000,7,10**(-25))
+    print space2
+
+
+    closestPair = closestPairBrutePlus(space2)
+    closestPair = closestPairDivide(space2)
 
 
 def closestPairBrutePlus(space):
@@ -55,16 +59,20 @@ def closestPairBrutePlus(space):
     lista = space.getClosestBrutePlus()
     time2 = time.time()
 
+    print "Fuerza Bruta: "
     print 'function took %0.3f s' % ( (time2-time1))
     print lista
+    return lista
 
 def closestPairDivide(space):
     time1 = time.time()
     lista = space.getClosestDivideConquer()
     time2 = time.time()
 
+    print "Divide y venceras: "
     print 'function took %0.3f s' % ( (time2-time1))
     print lista
+    return lista
 
 
 if __name__ == "__main__":
